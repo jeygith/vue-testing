105d49ea85e08383824d0c77b03c65f2c65daf35
Jeff Githiri <geoffrey.githiri@andela.com>
2019-11-06 18:00:50 +0300

    init lesson 3 - setting data and asserting against inline styles

---
+++ episode-3/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <span class="count" v-text="count"></span>
+        <button @click="count++" class="increment">Increment</button>
+
+        <button @click="count--" class="decrement">Decrement</button>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                count: 0
+            };
+        }
+    }
+</script>
